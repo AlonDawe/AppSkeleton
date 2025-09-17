9fe8471c186b9cdd9d1056f7c176b093d1940c72
Alon Dawe <alon.dawe@gmail.com>
2025-09-17 11:35:10 +0200

    Included login and signup placeholders with the new Protected option

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,6 +8,7 @@ import { useColorScheme } from '@/hooks/use-color-scheme';
 export const unstable_settings = {
   anchor: '(tabs)',
 };
+const isLoggedIn = false;
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -15,8 +16,22 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+          <Stack.Protected guard={isLoggedIn}>
+              <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
+          </Stack.Protected>
+          <Stack.Protected guard={isLoggedIn}>
+          </Stack.Protected>
+          <Stack.Protected guard={!isLoggedIn}>
+              <Stack.Screen name="sign-up" options={{ headerShown: false }} />
+              <Stack.Screen name="log-in" options={{ headerShown: false }} />
+              <Stack.Screen name="modal" options={{
+                  presentation: 'modal',
+                  title: 'Modal',
+                  headerShown: false }} />
+          </Stack.Protected>
+
+
+
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
---
+++ app/log-in.tsx
@@ -0,0 +1,46 @@
+import { StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+
+export default function LoginScreen() {
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedView style={styles.content}>
+        <ThemedText type="title">Log-In</ThemedText>
+        <ThemedText>App Log-In goes here.</ThemedText>
+        <ThemedView style={styles.linkContainer}>
+          <ThemedText>Don't have an account? </ThemedText>
+          <Link href="/sign-up">
+            <ThemedText type="link">Sign-up</ThemedText>
+          </Link>
+        </ThemedView>
+      </ThemedView>
+      <Link href="/modal" style={styles.privacyLink}>
+        <ThemedText type="link">Privacy Policy</ThemedText>
+      </Link>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 20,
+  },
+  content: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  linkContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  privacyLink: {
+    marginBottom: 20,
+  },
+});
