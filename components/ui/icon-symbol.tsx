4e424016220a9cf01f2a9f4278142b69001406cf
Alon Dawe <alon.dawe@gmail.com>
2025-09-18 15:53:06 +0200

    The drawer wasn;t showing up on android, quick fix

--- components/ui/icon-symbol.tsx
+++ components/ui/icon-symbol.tsx
@@ -14,13 +14,14 @@ type IconSymbolName = keyof typeof MAPPING;
  * - see SF Symbols in the [SF Symbols](https://developer.apple.com/sf-symbols/) app.
  */
 const MAPPING = {
-  'house.fill': 'home',
-  'paperplane.fill': 'send',
-  'chevron.left.forwardslash.chevron.right': 'code',
-  'chevron.right': 'chevron-right',
-  'envelope': 'email',
-  'lock': 'lock',
-  'person': 'person',
+    'house.fill': 'home',
+    'paperplane.fill': 'send',
+    'chevron.left.forwardslash.chevron.right': 'code',
+    'chevron.right': 'chevron-right',
+    'line.3.horizontal': 'menu',
+    'envelope': 'email',
+    'lock': 'lock',
+    'person': 'person',
 } as IconMapping;
 
 /**
